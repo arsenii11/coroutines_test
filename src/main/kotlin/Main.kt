0b606ec1dad66a746ee46dabd2b92ec9e68b9cc1
Arsenii Dragunkin <57676564+arsenii11@users.noreply.github.com>
2022-12-24 02:44:38 +0200

    Simple code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,17 +2,9 @@ import kotlinx.coroutines.*
 import kotlin.random.Random
 
 fun main(): Unit = runBlocking {
-    val couroutines: List<Deferred<String>> = List(100) {
-        async (start = CoroutineStart.ATOMIC){
-            doWork(it.toString())
-        }
+    launch {
+        delay(1000L)
+        println("World")
     }
-    couroutines.forEach{
-        println(it.await())
-    }
-}
-
-suspend fun doWork(name:String):String{
-    delay(Random.nextInt(5000).toLong())
-    return "Done. $name"
+    println("Hello")
 }
